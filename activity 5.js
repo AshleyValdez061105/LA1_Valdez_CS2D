9c1ea3cf43e6564319dcb9018884d0829bbfa494
AshleyValdez061105 <ashleyvaldez061105@gmail.com>
2024-09-23 08:40:52 +0800

    activity 5.js

---
+++ activity 5.js
@@ -0,0 +1,16 @@
+// Create variable name
+let favActorFirstName = "Gabriel";
+let favActorLastName = "Guevara";
+let fullName = favActorFirstName + " " + favActorLastName;
+let uppercase = fullName.toUpperCase();
+let message = `My favorite Actor Is ${uppercase}`;
+
+// Now append this message to the message variable `his best show is My fault`,
+message += ` his best show is My fault `;
+
+// log all variables to the console using code 
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(message);
