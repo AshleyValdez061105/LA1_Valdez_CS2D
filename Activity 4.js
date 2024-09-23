b6ffe2fd727fda5a734b63cfc2f5c286d3d1be53
AshleyValdez061105 <ashleyvaldez061105@gmail.com>
2024-09-23 09:32:58 +0800

    Activity 4.js

--- Activity 4.js
+++ Activity 4.js
@@ -1,4 +1,50 @@
-// create variable name & store your favorite number
+/// create variable name (firstFavNumb) & store your favorite number. 
+let firstFavNumb = 11;
+let secondFavNumb = 20;
+
+//check firstFavNumb is greater then secondFavNumb 
+let isGreater = firstFavNumb > secondFavNumb; 
+
+//check firstFavNumb is less then secondFavNumb
+
+let isLess = firstFavNumb < secondFavNumb; 
+
+// check firstFavNumb is greater then & equal to secondFavNumb
+
+let isGreaterOrEqual = firstFavNumb >= secondFavNumb;
+
+// check firstFavNumb is less then & equal to secondFavNumb
+
+let isLessOrEqual = firstFavNumb <= secondFavNumb;
+
+// check is equal to using strict equality operator
+
+let isStrictlyEqual = firstFavNumb === secondFavNumb; 
+
+// checkis equal to using loose equality operator
+
+let isLooselyEqual = firstFavNumb == secondFavNumb; 
+
+// check is not equal to using strict non-equality operator
+
+let isStrictlyNotEqual = firstFavNumb !== secondFavNumb;
+
+// check is not equal to secondFavNumb using loose non-equality operator
+
+let isLooselyNotEqual = firstFavNumb != secondFavNumb;
+
+// finally log all variables to the console using code 
+
+console.log("firstFavNumb:", firstFavNumb);
+console.log("secondFavNumb:", secondFavNumb);
+console.log("isGreater:", isGreater);
+console.log("isLess:", isLess);
+console.log("isGreaterOrEqual:", isGreaterOrEqual);
+console.log("isLessOrEqual:", isLessOrEqual);
+console.log("isStrictlyEqual:", isStrictlyEqual);
+console.log("isLooselyEqual:", isLooselyEqual);
+console.log("isStrictlyNotEqual:", isStrictlyNotEqual);
+console.log("isLooselyNotEqual:", isLooselyNotEqual);/ create variable name & store your favorite number
 
 let firstFavNumb = 11;
 let secondFavNumb = 20;
