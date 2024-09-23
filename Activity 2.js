6687e3fa7451dc038108d31417e3dc79f2e909b5
AshleyValdez061105 <ashleyvaldez061105@gmail.com>
2024-09-23 09:01:11 +0800

    Activity 2.js

---
+++ Activity 2.js
@@ -0,0 +1,32 @@
+// Create a Variable
+let firstFavNum = 11;
+let secondFavNum = 26; 
+
+// Addition
+// Add  (firstFavNun & secondFavNum)
+let additionResult = firstFavNum + secondFavNum;
+console.log('Addition:', additionResult);
+
+// Subtraction
+// Subtract (firstFavNum & secondFavNum)
+let subtractionResult = firstFavNum - secondFavNum;
+console.log('Subtraction:', subtractionResult);
+
+// Multiplication
+// Multiply (firstFavNum & secondFavNun)
+let multiplicationResult = firstFavNum * secondFavNum;
+console.log('Multiplication:', multiplicationResult);
+
+// Division
+// Divide (firstFavNun & secondFavNum)
+let divisionResult = firstFavNum / secondFavNum;
+console.log('Division:', divisionResult);
+
+// Modulus (Remainder)
+let modulusResult = firstFavNum % secondFavNum;
+console.log('Modulus:', modulusResult);
+
+// Exponentation (Power)
+// Check the power of (firstFavNum) raise to secondFavNun
+let exponentationResult = firstFavNum ** secondFavNum;
+console.log('Exponemtation', exponentationResult);
