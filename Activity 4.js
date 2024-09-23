ec9e1599b805671f238d588341e67e802dcf2cfe
AshleyValdez061105 <ashleyvaldez061105@gmail.com>
2024-09-23 08:56:52 +0800

    Activity 4.js

---
+++ Activity 4.js
@@ -0,0 +1,29 @@
+// create variable name & store your favorite number
+
+let firstFavNumb = 11;
+let secondFavNumb = 20;
+
+let isGreater = firstFavNumb > secondFavNumb; 
+let isLess = firstFavNumb < secondFavNumb; 
+let isGreaterOrEqual = firstFavNumb >= secondFavNumb;
+let isLessOrEqual = firstFavNumb <= secondFavNumb;
+let isStrictlyEqual = firstFavNumb === secondFavNumb; 
+let isLooselyEqual = firstFavNumb == secondFavNumb; 
+let isStrictlyNotEqual = firstFavNumb !== secondFavNumb;
+
+// check is not equal to secondFavNumb using loose non-equality operator
+
+let isLooselyNotEqual = firstFavNumb != secondFavNumb;
+
+// finally log all variables to the console using code 
+
+console.log("firstFavNumb:", firstFavNumb);
+console.log("secondFavNumb:", secondFavNumb);
+console.log("isGreater:", isGreater);
+console.log("isLess:", isLess);
+console.log("isGreaterOrEqual:", isGreaterOrEqual);
+console.log("isLessOrEqual:", isLessOrEqual);
+console.log("isStrictlyEqual:", isStrictlyEqual);
+console.log("isLooselyEqual:", isLooselyEqual);
+console.log("isStrictlyNotEqual:", isStrictlyNotEqual);
+console.log("isLooselyNotEqual:", isLooselyNotEqual);isLooselyNotEqual);
