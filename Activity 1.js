e4bd58e3a9b62e693a4c9c100368263bb781420d
AshleyValdez061105 <ashleyvaldez061105@gmail.com>
2024-09-23 09:02:36 +0800

    Activity 1.js

---
+++ Activity 1.js
@@ -0,0 +1,13 @@
+// Create variable name
+
+let name = "Ashley Valdez ";
+let WhatDoYouWannaBecomeInYourLife = "Programmer";
+let gender = "Female";
+let FacebookAcct = "Ashley Valdez ";
+
+// Log all variables to the console
+
+console.log ("Name: ", name );
+console.log ("WhatDoYouWannaBecomeInYourLife: ",WhatDoYouWannaBecomeInYourLife );
+console.log ("Gender: ",gender , );
+console.log ("Facebook Account: ",FacebookAcct );
