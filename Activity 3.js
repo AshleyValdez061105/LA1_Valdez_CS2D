ba69f03425333a5be957220e8de5a67ab28b2768
AshleyValdez061105 <ashleyvaldez061105@gmail.com>
2024-09-23 08:58:31 +0800

    Activity 3.js

---
+++ Activity 3.js
@@ -0,0 +1,21 @@
+// create a variable to store whether JavaScript is a programing languages 
+
+let isJsProgrammingLanguage = true;
+
+// create a variable to store whether JavaScript is hard
+
+let isJsHard = false;
+
+// Add favNumber with the value of undefined, which will result in NaN
+
+// create a variable to store my favorite number 
+
+let favNumber = 11;
+let result = favNumber + undefined;
+
+// log all variables to the console
+
+console.log ("isJsProgrammingLanguage:", isJsProgrammingLanguage);
+console.log ("isJsHard:", isJsHard);
+console.log ("favNumber:", favNumber);
+console.log ("result:", result);
